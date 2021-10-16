e0d0c11ec5f39d2035c3878bf6bb1f068dca9bcb
supsuphae <zksdbstjq@gmail.com>
2021-10-16 20:19:30 +0900

    +update baekjoon6_3052.java / baekjoon7_4344.java

---
+++ src/STAGE5/baekjoon6_3052.java
@@ -0,0 +1,18 @@
+//5-6.나머지 | 21.10.16 // https://www.acmicpc.net/problem/3052
+package STAGE5;
+
+import java.util.Scanner;
+
+public class baekjoon6_3052 {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int[] arr = new int[10];
+		int[] reminder = new int[arr.length];
+		for (int i = 0; i < arr.length; i ++) {
+			arr[i] = scanner.nextInt();
+
+		}
+		
+	}
+}
