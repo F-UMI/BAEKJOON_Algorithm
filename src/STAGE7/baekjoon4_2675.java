556d583d76802230b7cfb50d21bf27bb07022e63
supsuphae0 <zksdbstjq@gmail.com>
2021-10-17 20:06:01 +0900

    +update STAGE7

---
+++ src/STAGE7/baekjoon4_2675.java
@@ -0,0 +1,28 @@
+package STAGE7;
+
+import java.util.Scanner;
+
+/**
+ * fileName : baekjoon4_2675
+ * date : 2021-10-17
+ * description : 문자열 반복 | https://www.acmicpc.net/problem/2675
+ * ===========================================================
+ * DATE AUTHOR NOTE
+ * -----------------------------------------------------------
+ * 2021-10-17 CKIRUser 최초 생성
+ */
+public class baekjoon4_2675 {
+   public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int n = scanner.nextInt();
+      String[] s =  {};
+      for ( int i = 0; i < n; i++ ) {
+         int repeatNum  = scanner.nextInt(); 
+         String[] tmp = scanner.nextLine().split("");
+         for ( int j = 0; j < tmp.length * repeatNum; j++ ) {
+
+         }
+         
+      }
+   }
+}
