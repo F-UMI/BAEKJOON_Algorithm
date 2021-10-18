63a7b14da1ad9f1e47191e89eb3a129bb4a6c91e
zksdbstjq <zksdbstjq@gmail.com>
2021-10-18 15:44:37 +0900

    +create baekjoon6_1152

---
+++ src/STAGE7/baekjoon6_1152.java
@@ -0,0 +1,29 @@
+package STAGE7;
+
+/**
+ * packageName : STAGE7
+ * fileName : baekjoon6_1152
+ * author : Yunsup Shin
+ * date : 2021-10-18
+ * description :
+ * ===========================================================
+ * DATE AUTHOR NOTE
+ * -----------------------------------------------------------
+ * 2021-10-18 Yunsup Shin 최초 생성
+ */
+import java.util.Scanner;
+public class baekjoon6_1152 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String[] strings = s.split(" ");
+        int length = strings.length;
+        for ( int i = 0; i < length; i++ ) {
+            if ( strings[i].isEmpty()) {
+                length = strings.length -1 ;
+            }
+        }
+        System.out.println(length);
+
+    }
+}
