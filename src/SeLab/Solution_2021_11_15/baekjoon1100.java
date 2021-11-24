d7301936ce35b22c65388b09e81d4ee3fbbf5dda
윤섭 <zksdbstjq@gmail.com>
2021-11-24 20:55:30 +0900

    Update baekjoon1100.java

--- src/SeLab/Solution_2021_11_15/baekjoon1100.java
+++ src/SeLab/Solution_2021_11_15/baekjoon1100.java
@@ -1,7 +1,23 @@
 package SeLab.Solution_2021_11_15;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class baekjoon1100 {
+    public final static String[][] CHESSBOARD = new String[8][8];
+    public final static int ZERO = 0;
+    public final static String REGEX = ""
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int row = ZERO;
+        for (int i = 0; ; i++){
+           divideTheSpace(scanner.next(), CHESSBOARD);
+
+        }
 
     }
+    public static <CHESSBOARD> String divideTheSpace (String col, String[][] CHESSBOARD) {
+
+    }
+
 }
