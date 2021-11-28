a1e692a3d7ec5a9994536438f4ab123a322be9e4
yunsup <zksdbstjq@gmail.com>
2021-11-28 17:54:37 +0900

    Update : BackUp

---
+++ src/SeLab/Solution_2021_11_15/baekjoon11866_Queue.java
@@ -0,0 +1,35 @@
+package SeLab.Solution_2021_11_15;
+
+import java.util.*;
+
+public class baekjoon11866_Queue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Queue<Integer> permutation = new LinkedList<>();
+        offerPermutation(permutation, scanner.nextInt());
+        printJosephusPermutation(permutation, scanner.nextInt());
+        scanner.close();
+    }
+
+    public static void offerPermutation(Queue<Integer> queue, int peopleNumber) {
+        for (int i = 1; i <= peopleNumber; i++){
+            queue.offer(i);
+        }
+    }
+
+    public static void printJosephusPermutation(Queue<Integer> queue, int order) {
+        System.out.print("<");
+        while (!queue.isEmpty()) {
+            for (int i = 1; i <= order - 1; i++){
+                queue.offer(queue.poll());
+            }
+            System.out.print(queue.poll() + getIdentifier(queue));
+        }
+    }
+
+    public static String getIdentifier(Queue<Integer> queue) {
+        if(queue.isEmpty()) return ">";
+        return ", ";
+    }
+
+}
