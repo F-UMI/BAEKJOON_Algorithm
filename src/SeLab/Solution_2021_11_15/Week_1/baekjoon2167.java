e666a6cdeaf83f5af0812dfd994af19ea075b68d
yunsup <zksdbstjq@gmail.com>
2021-11-29 20:58:23 +0900

    Update backup

--- src/SeLab/Solution_2021_11_15/Week_1/baekjoon2167.java
+++ src/SeLab/Solution_2021_11_15/Week_1/baekjoon2167.java
@@ -5,55 +5,56 @@ import com.sun.org.apache.xerces.internal.impl.xs.SchemaNamespaceSupport;
 import java.util.Scanner;
 
 public class baekjoon2167 {
-    public static final int INIT_RANGE = 0;
-    public static final int GAP = 1;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int row = scanner.nextInt();
-        int col = scanner.nextInt();
-        int[][] array = new int[row][col];
-        scanArray(array, scanner);
-        int testCase = scanner.nextInt();
-        int[] testCaseResult = new int[testCase];
-        for (int k = 0; k < testCase; k++){
-            testCaseResult[k] = getSum(array, scanner, INIT_RANGE);
-        }
-        scanner.close();
-        System.out.println();
-        printResult(testCaseResult);
-    }
+  public static final int INIT_RANGE = 0;
+  public static final int GAP = 1;
 
-    private static void scanArray(int[][] array, Scanner scanner) {
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[0].length; j++){
-                array[i][j] = scanner.nextInt();
-            }
-        }
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int row = scanner.nextInt();
+    int col = scanner.nextInt();
+    int[][] array = new int[row][col];
+    scanArray(array, scanner);
+    int testCase = scanner.nextInt();
+    int[] testCaseResult = new int[testCase];
+    for (int k = 0; k < testCase; k++) {
+      testCaseResult[k] = getSum(array, scanner, INIT_RANGE);
     }
+    scanner.close();
+    System.out.println();
+    printResult(testCaseResult);
+  }
 
-    public static int getSum(int[][] array, Scanner scanner, int total) {
-        int i = scanNumber(scanner);
-        int j = scanNumber(scanner);
-        int x = scanNumber(scanner);
-        int y = scanNumber(scanner);
-        for (int k = i - 1; k < j - 1; k++){
-            for (int l = x - 1; l < y - 1; l++){
-                total += array[i][j];
-            }
-        }
-        return total;
+  private static void scanArray(int[][] array, Scanner scanner) {
+    for (int i = 0; i < array.length; i++) {
+      for (int j = 0; j < array[0].length; j++) {
+        array[i][j] = scanner.nextInt();
+      }
     }
+  }
 
-    public static int scanNumber(Scanner scanner) {
-        return scanner.nextInt() - GAP;
+  public static int getSum(int[][] array, Scanner scanner, int total) {
+    int i = scanNumber(scanner);
+    int j = scanNumber(scanner);
+    int x = scanNumber(scanner);
+    int y = scanNumber(scanner);
+    for (int k = i - 1; k < j - 1; k++) {
+      for (int l = x - 1; l < y - 1; l++) {
+        total += array[i][j];
+      }
     }
+    return total;
+  }
 
-    public static void printResult(int[] testCaseResult) {
-        for (int result : testCaseResult){
-            System.out.println(result);
-        }
+  public static int scanNumber(Scanner scanner) {
+    return scanner.nextInt() - GAP;
+  }
+
+  public static void printResult(int[] testCaseResult) {
+    for (int result : testCaseResult) {
+      System.out.println(result);
     }
+  }
 
 }
 
--- src/SeLab/Solution_2021_11_15/Week_2/beakjoon1152.java
+++ src/SeLab/Solution_2021_11_15/Week_2/beakjoon1152.java
@@ -2,24 +2,28 @@ package SeLab.Solution_2021_11_15.Week_2;
 
 import java.util.Scanner;
 
-// https://www.acmicpc.net/problem/1152
+/* 백준 1152번
+*
+* */
 public class beakjoon1152 {
-    public final static String REGEX = " ";
-    public final static int INIT_RANGE = 0;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] string = (scanner.nextLine()).split(REGEX);
-        System.out.println(countWord(string, INIT_RANGE));
-    }
+  public final static String REGEX = " ";
+  public final static int INIT_RANGE = 0;
+  public final static int REMOVE_SPACES = 1;
 
-    public static int countWord(String[] string, int length) {
-        for (int i = 0; i < string.length; i++){
-            if(string[i].isEmpty()) {
-                length = string.length-1;
-            };
-        }
-        return length;
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String[] string = (scanner.nextLine()).split(REGEX);
+    System.out.println(countWord(string, INIT_RANGE));
+  }
+
+  public static int countWord(String[] string, int numberOfCharacters) {
+    for (int i = 0; i < string.length; i++) {
+      if (string[i].isEmpty()) {
+        numberOfCharacters = string.length - REMOVE_SPACES;
+      }
     }
+    return numberOfCharacters;
+  }
 
 }
