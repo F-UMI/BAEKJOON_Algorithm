930cda29228fdd9e62191fdc25b88c497c89e449
윤섭 <zksdbstjq@gmail.com>
2022-02-25 22:38:28 +0900

    Solved : STAGE1 - 10926번

---
+++ src/Stage1/beakjoon10926.java
@@ -0,0 +1,11 @@
+package Stage1;
+
+import java.util.Scanner;
+
+public class beakjoon10926 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(scanner.nextLine() + "??!");
+  }
+}
