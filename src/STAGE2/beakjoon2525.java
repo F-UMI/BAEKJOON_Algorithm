b3586947de82fa64937561249d925383846e2c89
윤섭 <zksdbstjq@gmail.com>
2022-02-25 23:50:35 +0900

    Solved : STAGE1 - 2525번

---
+++ src/STAGE2/beakjoon2525.java
@@ -0,0 +1,28 @@
+package STAGE2;
+
+import java.util.Scanner;
+
+public class beakjoon2525 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int hour = scanner.nextInt();
+    int minute = scanner.nextInt();
+    int cookingTime = scanner.nextInt();
+    int[] answer = calculatingTime(hour,minute,cookingTime);
+    System.out.println(answer[0] + " " + answer[1]);
+  }
+
+  private static int[] calculatingTime(int hour, int minute, int cookingTime) {
+    minute += cookingTime % 60;
+    if (minute >= 60) {
+      hour++;
+      minute = minute - 60;
+    }
+    hour += cookingTime / 60;
+    if (hour >= 24) {
+      hour = hour - 24;
+    }
+    return new int[]{hour, minute};
+  }
+}
