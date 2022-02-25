01829e5ccc6b329a4ffec2c737bd28056c57ad66
윤섭 <zksdbstjq@gmail.com>
2022-02-25 22:46:17 +0900

    Solved : STAGE1 - 18108번

---
+++ src/Stage1/beakjoon18108.java
@@ -0,0 +1,11 @@
+package Stage1;
+
+import java.util.Scanner;
+
+public class beakjoon18108 {
+
+  private static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(scanner.nextInt() - 543);
+  }
+}
