533f081bc6b952d610d874825ae1be0ca103651d
윤섭 <zksdbstjq@gmail.com>
2022-02-28 00:11:53 +0900

    Solving : STAGE7 - 2675번

--- src/STAGE7/baekjoon4_2675.java
+++ src/STAGE7/baekjoon4_2675.java
@@ -1,42 +1,42 @@
 package STAGE7;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
- * fileName : baekjoon4_2675
- * date : 2021-10-17
- * description : 문자열 반복 | https://www.acmicpc.net/problem/2675
- * ===========================================================
- * DATE AUTHOR NOTE
- * -----------------------------------------------------------
- * 2021-10-17 CKIRUser 최초 생성
- * 2021-10-18 F-UMI 크기가 5인데  왜 출력 4문자 반복시 오류 x? ex) 3 /f = ///fff
- * */
+ * fileName : baekjoon4_2675 date : 2021-10-17 description : 문자열 반복 |
+ * https://www.acmicpc.net/problem/2675 ===========================================================
+ * DATE AUTHOR NOTE ----------------------------------------------------------- 2021-10-17 CKIRUser
+ * 최초 생성 2021-10-18 F-UMI 크기가 5인데  왜 출력 4문자 반복시 오류 x? ex) 3 /f = ///fff
+ */
 public class baekjoon4_2675 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        String answer = "";
-        int[] repeatNum = new int[n];
-        String[] tmp = new String[n];
-        for ( int i = 0; i < n; i++ ) {
-            repeatNum[i] = scanner.nextInt();
-            tmp[i] = scanner.next();
+        int count = scanner.nextInt();
+        List<String> list = new ArrayList<>(count);
+        for (int i = 0; i < count; i++) {
+            System.out.println(stringCreation(scanner.nextInt(), scanner.next()));
         }
-        for ( int i = 0; i < n; i++ ) {
-            for ( int j = 0; j < repeatNum[i]; j++ ) {
-                for ( int k = 0; k < repeatNum[i]; k++ ) {
-                    try {
-                        System.out.print(tmp[i].charAt(j));
-                    } catch ( Exception e ) {
-                        System.out.print(" .");
-                    }
-                }
-            }
-            System.out.println("");
 
-        }
     }
 
+    private static String stringCreation(int nextInt, String next) {
+        String[] tmp = next.split("");
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] += characterRepetition(nextInt, tmp[i]);
+        }
+        return String.join("", tmp);
+    }
 
+    private static String characterRepetition(int nextInt, String next) {
+        for (int i = 0; i < nextInt; i++) {
+            next += next;
+        }
+        return next;
+    }
 }
+
+
