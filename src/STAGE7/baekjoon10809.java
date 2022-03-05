81e166313c10a4c51468098c0cd0d8f427221c43
윤섭 <zksdbstjq@gmail.com>
2022-03-05 23:42:37 +0900

    Solving : STAGE7- 10809번 풀이중

---
+++ src/STAGE7/baekjoon10809.java
@@ -0,0 +1,24 @@
+package STAGE7;
+
+import java.util.Scanner;
+
+public class baekjoon10809 {
+  final static int NUMBER_OF_ALPHABETS = 26;
+
+
+  public static void main(String[] args) {
+    int[] alphabet = new int[NUMBER_OF_ALPHABETS];
+    Scanner scanner = new Scanner(System.in);
+    String s = scanner.next();
+    for (int i = 0; i < NUMBER_OF_ALPHABETS; i++) {
+      alphabet[i] = findIndexOfAlphabet(s);
+    }
+  }
+
+  private static int findIndexOfAlphabet(String s) {
+    for (int d = 97; d < 97 + NUMBER_OF_ALPHABETS; d++) {
+      if ()
+    }
+  }
+  97 -122
+}
